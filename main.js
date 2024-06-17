cabdda52140ce338725b64f795cf2550724dfaff
Suryadeep36 <gohilsuryadeep3101@gmail.com>
2024-06-17 20:23:05 +0530

    add 250 random stars, change some css

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 import * as THREE from 'three';
-import { vec3 } from 'three/examples/jsm/nodes/Nodes.js';
+
 //scene settings
 const scene =  new THREE.Scene();
 const backgroundTexture = new THREE.TextureLoader().load( "./space.jpeg" );
@@ -7,6 +7,7 @@ scene.background = backgroundTexture;
 
 //camera settings
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
+camera.aspect= window.innerWidth/window.innerHeight;
 camera.position.z = 25;
 
 //renderer settings
@@ -26,7 +27,8 @@ scene.add( directionalLight);
 // const directionalLightHelper = new THREE.DirectionalLightHelper(directionalLight);
 // scene.add(directionalLightHelper);
 
-
+//adding 250 stars
+Array(250).fill().forEach(addStar)
 
 
 //sun model
@@ -35,7 +37,8 @@ const sunMaterial = new THREE.MeshStandardMaterial({
     map: new THREE.TextureLoader().load('./sun.jpg')
 })
 const sun = new THREE.Mesh( sunGeometry, sunMaterial ); 
-let sunInitPosX = 10;
+let sunInitPosX = 15;
+sun.position.z = -30;
 sun.position.x = sunInitPosX;
 scene.add( sun );
 
@@ -55,5 +58,31 @@ function renderStuff(){
 function moveCamera(){
     let t = document.body.getBoundingClientRect().top;
     sun.position.x =  (t * 0.01) + sunInitPosX;
-    sun.position.z = t*-0.02;
+    camera.position.z = t * 0.02
 }
+
+//this function will add star to random place in the space
+function addStar(){
+    const starGeomatry = new THREE.SphereGeometry(0.15, 26, 26);
+    const starMaterial = new THREE.MeshStandardMaterial({
+        color: "yellow"
+    })
+    const star = new THREE.Mesh(starGeomatry, starMaterial);
+    const [x, y, z] = Array(3).fill().map(()=> THREE.MathUtils.randFloatSpread(100));
+    star.position.set(x, y, z);
+    scene.add(star);
+}
+
+
+// function to handle window resize
+function handleResize() {
+    const width = window.innerWidth;
+    const height = window.innerHeight;
+    camera.aspect = width / height;
+    camera.updateProjectionMatrix();
+    renderer.setSize(width, height);
+    scene.background = backgroundTexture;
+}
+
+// add event listener for window resize
+window.addEventListener('resize', handleResize);
