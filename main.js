0e3a108e92d4200e2bb4e1bc27418ab7023440cf
Suryadeep36 <gohilsuryadeep3101@gmail.com>
2024-06-19 16:31:30 +0530

    add remaining planets only content is remaining now, also make site a little responsive

--- main.js
+++ main.js
@@ -1,16 +1,12 @@
 import * as THREE from 'three';
 
-
-//validation to check if screen is in landscape
-console.log();
+//validations for landscape mode
 if(screen.orientation.type == "portrait-primary" || screen.orientation.type == "portrait-secondary"){
     stuffToDoIfNotLandScape();
 }
 
 screen.orientation.addEventListener('change',(event)=>{
     const type = event.target.type;
-    const angle = event.target.angle;
-    console.log(`ScreenOrientation change: ${type}, ${angle} degrees.`);
     if(type == "portrait-primary" || type == "portrait-secondary"){
         stuffToDoIfNotLandScape();
     }
@@ -50,16 +46,122 @@ scene.add( directionalLight);
 Array(250).fill().forEach(addStar)
 
 
+
 //sun model
-const sunGeometry = new THREE.SphereGeometry(5, 32, 16);
+const sunGeometry = new THREE.SphereGeometry(15, 32, 16);
 const sunMaterial = new THREE.MeshStandardMaterial({
     map: new THREE.TextureLoader().load('./sun.jpg')
 })
 const sun = new THREE.Mesh( sunGeometry, sunMaterial ); 
-let sunInitPosX = 15;
-sun.position.z = -30;
-sun.position.x = sunInitPosX;
+sun.position.z = -50;
+sun.position.x = 25;
 scene.add( sun );
+
+//mercury model
+const mercuryGeometry = new THREE.SphereGeometry(1, 32, 16);
+const mercuryMaterial = new THREE.MeshStandardMaterial({
+    map: new THREE.TextureLoader().load('./mercury.jpg')
+})
+const mercury = new THREE.Mesh(mercuryGeometry, mercuryMaterial);
+mercury.position.x = 35;
+mercury.position.z = -70;
+scene.add(mercury);
+
+//venus model
+const venusGeometry = new THREE.SphereGeometry(3, 32, 16);
+const venusMaterial = new THREE.MeshStandardMaterial({
+    map: new THREE.TextureLoader().load('./venus.jpg')
+})
+const venus = new THREE.Mesh(venusGeometry, venusMaterial);
+venus.position.x = 45;
+venus.position.z = -90;
+scene.add(venus);
+
+//earth model
+const earthGeometry = new THREE.SphereGeometry(3, 32, 16);
+const earthMaterial = new THREE.MeshStandardMaterial({
+    map: new THREE.TextureLoader().load('./earth.jpg')
+})
+const earth = new THREE.Mesh(earthGeometry,earthMaterial);
+earth.position.x = 55;
+earth.position.z = -110;
+scene.add(earth);
+
+//mars model
+const marsGeometry = new THREE.SphereGeometry(2, 32, 16);
+const marsMaterial = new THREE.MeshStandardMaterial({
+    map: new THREE.TextureLoader().load('./mars.jpg')
+})
+const mars = new THREE.Mesh(marsGeometry,marsMaterial);
+mars.position.x = 70;
+mars.position.z = -140;
+scene.add(mars);
+
+//jupital model
+const jupitalGeometry = new THREE.SphereGeometry(7, 32, 16);
+const jupitalMaterial = new THREE.MeshStandardMaterial({
+    map: new THREE.TextureLoader().load('./jupiter.jpg')
+})
+const jupital = new THREE.Mesh(jupitalGeometry,jupitalMaterial);
+jupital.position.x = 150;
+jupital.position.z = -300;
+scene.add(jupital);
+
+//saturn model
+const saturnGeometry = new THREE.SphereGeometry(6, 32, 16);
+const saturnMaterial = new THREE.MeshStandardMaterial({
+    map: new THREE.TextureLoader().load('./saturn.jpg')
+})
+const saturn = new THREE.Mesh(saturnGeometry,saturnMaterial);
+saturn.position.x = 200;
+saturn.position.z = -400;
+scene.add(saturn);
+
+//saturn rings model
+const saturnRingGeometry = new THREE.RingGeometry(7, 12, 32);
+const saturnRingMaterial = new THREE.MeshBasicMaterial({
+    map: new THREE.TextureLoader().load('./saturn_rings.jpg'),
+    transparent: true,
+    side: THREE.DoubleSide
+})
+const saturnRings = new THREE.Mesh(saturnRingGeometry, saturnRingMaterial);
+saturnRings.position.x = 200;
+saturnRings.position.z = -400;
+saturnRings.rotateX(2);
+scene.add(saturnRings);
+
+//fix for the texture of saturn's ring
+let pos = saturnRingGeometry.attributes.position;
+let uv = saturnRingGeometry.attributes.uv;
+let v3 = new THREE.Vector3();
+let v2 = new THREE.Vector2();
+for (let i = 0; i < pos.count; i++) {
+    v3.fromBufferAttribute(pos, i);
+    let angle = Math.atan2(v3.y, v3.x) / (2 * Math.PI);
+    angle = (angle + 1) / 2;
+    v2.set(angle, v3.z > 0 ? 0 : 1);
+    uv.setXY(i, v2.x, v2.y);
+}
+
+//uranus model
+const uranusGeometry = new THREE.SphereGeometry(5, 32, 16);
+const uranusMaterial = new THREE.MeshStandardMaterial({
+    map: new THREE.TextureLoader().load('./uranus.jpg')
+})
+const uranus = new THREE.Mesh(uranusGeometry,uranusMaterial);
+uranus.position.x = 250;
+uranus.position.z = -500;
+scene.add(uranus);
+
+//neptune model
+const neptuneGeometry = new THREE.SphereGeometry(5, 32, 16);
+const neptuneMaterial = new THREE.MeshStandardMaterial({
+    map: new THREE.TextureLoader().load('./neptune.jpg')
+})
+const neptune = new THREE.Mesh(neptuneGeometry,neptuneMaterial);
+neptune.position.x = 300;
+neptune.position.z = -600;
+scene.add(neptune);
 
 //scroll
 document.body.onscroll = moveCamera;
@@ -68,16 +170,25 @@ renderer.setAnimationLoop(renderStuff);
 
 //function to animate everything
 function renderStuff(){
-    sun.rotateX(0.004);
-    sun.rotateY(0.002);
+    sun.rotateY(0.02);
+    mercury.rotateY(0.098);
+    venus.rotateY(0.2);
+    earth.rotateY(0.02);
+    mars.rotateY(0.02);
+    jupital.rotateY(0.008);
+    saturn.rotateY(0.009);
+    saturnRings.rotateZ(0.009);
+    uranus.rotateY(0.015);
+    neptune.rotateY(0.013);
     renderer.render( scene, camera );
 }
 
 //this module will help when user scrolls
 function moveCamera(){
     let t = document.body.getBoundingClientRect().top;
-    sun.position.x =  (t * 0.01) + sunInitPosX;
-    camera.position.z = t * 0.02
+    // sun.position.x =  (t * 0.01) + sunInitPosX;
+    camera.position.z = t * 0.02;
+    camera.position.x = -t * 0.01;
 }
 
 //this function will add star to random place in the space
@@ -106,7 +217,7 @@ function handleResize() {
 // add event listener for window resize
 window.addEventListener('resize', handleResize);
 
-
+//modules for landscape mode
 function stuffToDoIfNotLandScape(){
     document.body.style.visibility = 'hidden';
     let ele = document.getElementById('warning')
